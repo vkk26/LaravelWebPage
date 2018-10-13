e0164de72e0c57ab6597391db91ad37da19fb2c6
Vkk26 <vkk26@njit.edu>
2018-10-13 19:04:30 -0400

    Added body.blade.php

---
+++ resources/views/includes/body.blade.php
@@ -0,0 +1,7 @@
+<body>
+<h1>about us</h1>
+
+
+<p>Our Chefs are not hired, they are handpicked and nurtured just like an Indian farmer, diligently would nurture the grains of exotic Basmati Rice. They are chosen on the basis of pedigree, extensive experience and their penchant for creating an exceptional dish out of ingredients commonly found in an Indian kitchen more like a bank would choose its senior most executive.
+    Why do we do that? Why would you think we go to such lengths to find the correct set of culinary talent? The answer to that would be our utmost commitment to delivering the best there is in Indian food. This also highlights why we are inclined towards serving food that has worldwide recognition</p>
+</body>
