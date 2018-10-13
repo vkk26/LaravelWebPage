49f75c462fda498efbefcd1fd610d26c9d85296f
Vkk26 <vkk26@njit.edu>
2018-10-13 19:04:53 -0400

    Added connect.blade.php

---
+++ resources/views/includes/connect.blade.php
@@ -0,0 +1,14 @@
+<div class="container-fluid padding">
+    <div class="row text-center padding">
+        <div class="col-6">
+            <h5> contact us</h5>
+        </div>
+        <div class="col-6 social padding">
+            <a href="https://www.facebook.com/DeccanSpice/"><i class="fab fa-facebook"></i></a>
+            <a href="https://www.youtube.com/watch?v=idFbzyz0aT4"><i class="fab fa-youtube"></i></a>
+            <a href="https://twitter.com/deccanspice?lang=en"><i class="fab fa-twitter"></i></a>
+            <a href="https://www.instagram.com/deccanspice/?hl=en"><i class="fab fa-instagram"></i></a>
+
+        </div>
+    </div>
+</div>
