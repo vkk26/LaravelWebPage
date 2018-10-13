8a5dee488af5c72a0d0e46d141e4cf90dd6e6f48
Vkk26 <vkk26@njit.edu>
2018-10-13 17:58:29 -0400

    first commit

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,33 @@
+<header>
+    <nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Deccanspice</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="https://www.deccanspice.com/">Home</a>
+
+
+                    </li><li class="nav-item ">
+                        <a class="nav-link" href="https://www.deccanspice.com/about-us/">About Us</a>
+
+
+                    </li><li class="nav-item ">
+                        <a class="nav-link" href="https://www.deccanspice.com/menu/">Menu</a>
+
+
+                    </li><li class="nav-item ">
+                        <a class="nav-link" href="https://www.deccanspice.com/contact/">Contact</a>
+
+
+                    </li>
+
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+</header>
